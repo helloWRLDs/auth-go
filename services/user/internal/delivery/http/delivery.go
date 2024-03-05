7bf610f611c098fb53b7a4e05d09bdff8d7b17f2
Danil Li <danil.li24x@gmail.com>
2024-03-06 00:01:53 +0500

    router

--- services/user/internal/delivery/http/delivery.go
+++ services/user/internal/delivery/http/delivery.go
@@ -24,6 +24,13 @@ func (d *HttpDelivery) InitRoutes() {
 	d.Router.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, "pong")
 	})
-
 	d.Router.POST("/signup", d.UserUC.RegisterUser)
+	d.Router.POST("/login", d.UserUC.LoginUser)
+	d.Router.Group("/user")
+	{
+		d.Router.GET("/", d.UserUC.GetUsers)
+		d.Router.GET("/:id", d.UserUC.GetUser)
+		d.Router.PUT("/:id", d.UserUC.UpdateUser)
+		d.Router.DELETE("/:id", d.UserUC.RemoveUser)
+	}
 }
